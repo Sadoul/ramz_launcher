feef973caec32593761fdc60e06973f4312bab68
smopo <smopozkin@gmail.com>
2026-06-19 10:40:32 +0300

    Коммит 25

--- check_events.ps1
+++
@@ -1,4 +0,0 @@
-Get-WinEvent -FilterHashtable @{LogName='Application'; StartTime=(Get-Date).AddMinutes(-15)} -ErrorAction SilentlyContinue |
-  Where-Object { $_.Message -match 'pd-launcher|doomsday|WebView|ramz' } |
-  Select-Object -First 10 TimeCreated, LevelDisplayName, Message |
-  Format-List
